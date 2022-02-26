8a594349a8ad139a758efa89d408652ed6dc1b43
Doxygen CI <ci-arduino@invalid>
2022-02-26 09:59:43 +0000

    Deploy docs to GitHub Pages from commit 7379402c70
    Commit: 7379402c703f30a66b974baee23cb41315810edf
    GitHub Actions run: 1902733663

--- html/search/functions_9.js
+++ html/search/functions_9.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['remove',['remove',['../class_abstract_list.html#a071af49ccdd54f39ba37eeb7ecd1cb08',1,'AbstractList::remove()'],['../class_double_linked_list.html#a79f4630f082e943110b9777838698fc2',1,'DoubleLinkedList::remove()'],['../class_single_linked_list.html#af86d0e54c9fc2581e2cd196c7ade6d04',1,'SingleLinkedList::remove()']]],
   ['removeall',['removeAll',['../class_abstract_list.html#a5bd9c7f78190cda9d47e5e2e5905af4b',1,'AbstractList']]],
+  ['removefirst',['removeFirst',['../class_abstract_list.html#aef932a451cea115323da713416eec20f',1,'AbstractList']]],
+  ['removelast',['removeLast',['../class_abstract_list.html#a4d9d9a04fd3921cb3d21d58986f2897a',1,'AbstractList']]],
   ['resetsize',['resetSize',['../class_abstract_list.html#abdf601852ec4c3a055e900bb5526fe66',1,'AbstractList']]]
 ];
